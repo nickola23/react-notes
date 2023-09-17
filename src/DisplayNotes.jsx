2326acc63745a094b5db27feb742e136f0944837
nickola23 <nikolamilanovic2305@gmail.com>
2023-09-17 20:08:13 +0200

    upgrade but not working :(

--- src/DisplayNotes.jsx
+++ src/DisplayNotes.jsx
@@ -1,9 +1,13 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import Note from './Note';
 import './notes.css';
-var notes =[
-  {id: 0, title: "Title here", text: "Description goes here"}
-]
+
+
+const [notes, setNotes] = useState([
+  {id: 0, title: "Title here", text: "Description goes here"}, 
+  {id: 1, title: "Title maybe", text: "Type whatever you want"}
+]);
 
 function Notes() {
     
@@ -11,17 +15,18 @@ function Notes() {
 
 export function DisplayNotes(){
     return (
-        <>
-            {notes.map((note) => <Note key={note.id} title={note.title} text={note.text}/>)}
-        </>
+      <>
+        <div className="notesMain">
+          {notes.map((note) => <Note key={note.id} title={note.title} text={note.text}/>)}
+        </div>
+        <div className='addNote' onClick={ addNote }>
+          +
+        </div>
+      </>
       );
 }
 
-export function addNote(){
-  var i = 0;
-  notes.forEach(element => {
-    i++;
-  });
-  notes.push(...notes, { id: i, title: "Type here", text: "and here"});
-  DisplayNotes()
+const addNote = () => {
+  const newNote = { id: notes.length + 1, title: 'Type here', text: 'and here' };
+  setNotes([...notes, newNote]);
 }
--- src/Note.jsx
+++ src/Note.jsx
@@ -5,21 +5,16 @@ import './notes.css';
 function Note(props) {
     return (
       <>
-      <div className="notesMain">
-          <div className="note">
-              <div className="noteHeader">
-                  <img className='noteLogo' src={logo} alt="React logo" />
-                  <h2 className="noteTitle">{ props.title }</h2>
-                  <div className="noteClose">X</div>
-              </div>
-              <div className="noteBody">
-                  <p className='noteText' contentEditable="true" >{ props.text }</p>
-              </div>
-          </div>
-      </div>
-      <div className='addNote' onClick={ addNote() }>
-          +
-      </div>
+        <div className="note">
+            <div className="noteHeader">
+                <img className='noteLogo' src={logo} alt="React logo" />
+                <h2 className="noteTitle">{ props.title }</h2>
+                <div className="noteClose">X</div>
+            </div>
+            <div className="noteBody">
+                <p className='noteText' contentEditable="true" >{ props.text }</p>
+            </div>
+        </div>
       </>
       
     );
