9fb238e8e8b10992021d48c51135d56f6f56228f
nickola23 <nikolamilanovic2305@gmail.com>
2023-09-18 18:48:24 +0200

    footer finished and icons added

--- src/DisplayNotes.jsx
+++ src/DisplayNotes.jsx
@@ -6,11 +6,11 @@ import './notes.css';
 
 export function DisplayNotes(){
   const [notes, setNotes] = useState([
-    {id: 0, title: "Title here", text: "Description goes here"}, 
-    {id: 1, title: "Title maybe", text: "Type whatever you want"}
+    {id: 0, title: "Title here", text: "Description goes here", date: new Date()}, 
+    {id: 1, title: "Title maybe", text: "Type whatever you want", date: new Date()}
   ]);
   const addNote = () => {
-    const newNote = { id: notes.length + 1, title: 'Type here', text: 'and here' };
+    const newNote = { id: notes.length + 1, title: 'Type here', text: 'and here', date: new Date()};
     setNotes([...notes, newNote]);
   }
 
@@ -22,7 +22,7 @@ export function DisplayNotes(){
     return (
       <>
         <div className="notesMain">
-          {notes.map((note) => <Note key={note.id} title={note.title} text={note.text}/>)}
+          {notes.map((note) => <Note key={note.id} title={note.title} text={note.text} date={note.date.toLocaleDateString('en-GB')} time={String(note.date.getHours()) + ":" + String(note.date.getMinutes()) + ":" + String(note.date.getSeconds())}/>)}
         </div>
         <div className='addNote' onClick={ addNote }>
           +
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { FaX } from "react-icons/fa6";
 import { addNote } from './DisplayNotes';
 import './notes.css';
 
@@ -9,18 +10,21 @@ function Note(props) {
             <div className="noteHeader">
                 <img className='noteLogo' src={logo} alt="React logo" />
                 <h2 className="noteTitle">{ props.title }</h2>
-                <div className="noteClose">X</div>
+                <div className="noteClose">
+                  <FaX />
+                </div>
             </div>
             <div className="noteBody">
                 <p className='noteText' contentEditable="true" >{ props.text }</p>
             </div>
             <div className="noteFooter">
-              <div className="noteStats">
-kjhgjkhg
+            <div className="noteDate">
+                  { props.date }
               </div>
-              <div className="noteDate">
-kjhjhg
+              <div className="noteTime">
+                  { props.time }
               </div>
+              
             </div>
         </div>
       </>
